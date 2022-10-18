546445b1743de68e15ebf767ae3ef205148f8ed0
teighmy22 <89541954+teighmy22@users.noreply.github.com>
2022-10-18 08:35:25 -0400

    Add files via upload

---
+++ webring_project3/script.js
@@ -0,0 +1,17 @@
+
+function randomlinks(){
+    var myrandom=Math.round(Math.random()*10)
+    var links=new Array()
+    links[0]="https://elenikary.github.io/project3_web_ring/"
+    links[1]="https://ayc325.github.io/DES070/Project3/index.html"
+    links[2]="https://avm325.github.io/WebRingProject/"
+    links[3]="https://williammorano.github.io/DES-070/html/webring.html"
+    links[4]="https://clarejachim.github.io/webring/"
+    links[5]="http://www.cse.lehigh.edu/~haz323/WebDesign/Project%203/index.html"
+    links[6]="https://msh223.github.io/WebRing/"
+    links[7]="https://mlk224.github.io/WebRing/"
+    links[8]="https://katiacarnevale.github.io/WebRing/"
+    links[9]="https://ctcooper11.github.io/Web-Ring/Web-Ring.html"
+ window.location=links[myrandom]
+  
+}
